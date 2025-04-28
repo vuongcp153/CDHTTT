1beba8eacfd4b927c8008665924e8dbcd4b30c56
Vuong <vuongcp153@gmail.com>
2025-04-28 19:21:55 +0700

    init

--- models/user.py
+++ models/user.py
@@ -1,14 +1,18 @@
 from datetime import datetime
 
 class User:
-    def __init__(self, data, is_matched=False):
+    def __init__(self,session, data, preference, status):
+        self.session = session
         self.data = data
-        self.timestamp = datetime.now()
-        self.is_matched = is_matched
+        self.preference = preference
+        self.last_activity = datetime.now()
+        self.status = status
 
     def to_dict(self):
         return {
+            "session_id": self.session,
             "data": self.data,
-            "timestamp": self.timestamp,
-            "is_matched": self.is_matched
+            "preference": self.preference,
+            "last_activity": self.last_activity,
+            "status": self.status
         }
